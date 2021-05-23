dc1445ee52a996d7bb6357927c71f6d04e1406b7
appubabu2 <84652726+appubabu2@users.noreply.github.com>
2021-05-23 11:15:53 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '💫 Join Our Group', url="https://t.me/CrazyBotsz"
                                 )
                         ]
                     ]
