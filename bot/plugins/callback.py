4856e794fe08e3d96c3fc1b9fcd457859802a20d
allyshe <83583332+allyshe@users.noreply.github.com>
2021-06-16 15:44:17 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code> hii
+    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Query:</i> <code>{query}</code> 👇 \n\n<b>© 𝐓𝐄𝐀𝐌 𝐅𝐈𝐋𝐌 𝐖𝐎𝐑𝐋𝐃</b>"
         
     try:
         await update.message.edit(
